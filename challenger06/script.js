002ba62bf178e8588c7e4a7621443d248c0cf2ba
Alexsandro Gomes dias <126702081+Phospudim@users.noreply.github.com>
2023-07-13 16:52:03 -0300

    update project

--- challenger06/script.js
+++ challenger06/script.js
@@ -1,13 +1,8 @@
-// Selecionar todos os botões
 var botoes = document.querySelectorAll('.faq-item button');
 
-// Adicionar um evento de clique a cada botão
 botoes.forEach(function(botao) {
   botao.addEventListener('click', function() {
-    // Selecionar a resposta correspondente ao botão clicado
-    var resposta = botao.nextElementSibling;
-    
-    // Alternar a exibição da resposta
+    var resposta = botao.parentElement.nextElementSibling;
     resposta.style.display = resposta.style.display === 'none' ? 'block' : 'none';
   });
 });
