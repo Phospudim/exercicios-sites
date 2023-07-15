6888440b51db36b3ec10d89fd079559cad0f022d
Alexsandro Gomes dias <126702081+Phospudim@users.noreply.github.com>
2023-07-14 21:59:48 -0300

    update

--- challenger06/script.js
+++ challenger06/script.js
@@ -1,8 +1,19 @@
 var botoes = document.querySelectorAll('.faq-item button');
+var arrows = document.querySelectorAll('.icon-container');
 
 botoes.forEach(function(botao) {
   botao.addEventListener('click', function() {
     var resposta = botao.parentElement.nextElementSibling;
+    var arrow = botao.querySelector('.icon-container');
+
     resposta.style.display = resposta.style.display === 'none' ? 'block' : 'none';
+
+    if (botao.classList.contains('pressed')) {
+      arrow.style.transform = 'rotate(0deg)'; 
+    } else {
+      arrow.style.transform = 'rotate(-180deg)';
+    }
+
+    botao.classList.toggle('pressed');
   });
 });
